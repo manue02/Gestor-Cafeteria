33ae14d4ff0cdf250168ed2320f2bbae7b5a40f6
manue02 <manu2002.03@gmail.com>
2024-06-05 18:34:28 +0200

    Actualizar estilos CSS y agregar contenido al menú

---
+++ js/contenidoMenu.js
@@ -0,0 +1,73 @@
+const navItems = [
+	{
+		title: "Inicio",
+		label: "Inicio",
+		url: "/index.html",
+	},
+	{
+		title: "Productos",
+		label: "Productos",
+		url: "/educacion",
+	},
+	{
+		title: "Categoria",
+		label: "Categoria",
+		url: "/proyectos",
+	},
+	{
+		title: "Cuentas",
+		label: "Cuentas",
+		url: "/#sobre-mi",
+	},
+];
+
+const nav = document.getElementById("nav");
+
+navItems.forEach((link) => {
+	const a = document.createElement("a");
+	a.className = "enlaces";
+	a.setAttribute("aria-label", link.label);
+	a.href = link.url;
+	a.textContent = link.title;
+
+	nav.appendChild(a);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	const sections = document.querySelectorAll("section");
+	const navItems = document.querySelectorAll("header nav a");
+
+	const callback = (entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				navItems.forEach((item) => {
+					if (item.getAttribute("aria-label") == entry.target.id) {
+						item.classList.add("text-yellow-500");
+					} else {
+						item.classList.remove("text-yellow-500");
+					}
+				});
+			}
+		});
+	};
+
+	const observer = new IntersectionObserver(callback, {
+		root: null,
+		rootMargin: "0px",
+		threshold: 0.3,
+	});
+
+	sections.forEach((section) => {
+		observer.observe(section);
+	});
+
+	document.onvisibilitychange = () => {
+		if (document.visibilityState === "hidden") {
+			observer.disconnect();
+		} else {
+			sections.forEach((section) => {
+				observer.observe(section);
+			});
+		}
+	};
+});
